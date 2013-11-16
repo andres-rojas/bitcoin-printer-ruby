98498a4e3591d5d4a2308bab0a83ecb343fb8b95
Andres Rojas <andres.rojas@mtnsat.com>
2013-11-16 17:51:14 -0500

    Prepare BTC keys and address

--- paper_bitcoin.rb
+++ paper_bitcoin.rb
@@ -2,3 +2,10 @@
 
 amount = ARGV.empty? ? '0.001' : "#{ARGV.first.to_f.round(3)}"
 amount += ' BTC'
+
+key = Bitcoin::generate_key
+secret = key[0]
+pubkey = key[1]
+addr_58 = Bitcoin::pubkey_to_address(pubkey)
+secreta = secret[0, 32]
+secretb = secret[32..-1]
