d637bf19c7050aca22239da88b8b5ca5a439539c
Andres Rojas <andres.rojas@mtnsat.com>
2013-11-16 18:58:36 -0500

    Added rotated_qr function

--- paper_bitcoin.rb
+++ paper_bitcoin.rb
@@ -9,3 +9,14 @@ pubkey = key[1]
 addr_58 = Bitcoin::pubkey_to_address(pubkey)
 secreta = secret[0, 32]
 secretb = secret[32..-1]
+
+## Given a string, returns a transparent QR code image object 
+## that has been rotated and resized to fit the template
+def rotated_qr(s)
+  derp = RQRCode::QRCode.new(s)
+  i = derp.draw(20, 'black', 'transparent')
+  i.rotate! 315
+  i.resize! 1100, 1100
+end
+
+
